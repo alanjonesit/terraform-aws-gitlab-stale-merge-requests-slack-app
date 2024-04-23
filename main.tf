6c3d030148034a90a88b740917004fa6acdb6bb9
Alan Jones <alanjonesit@outlook.com>
2024-04-23 10:03:47 +1000

    chore: remove unused variable

--- main.tf
+++ main.tf
@@ -149,8 +149,6 @@ resource "aws_cloudwatch_log_group" "lambda" {
   name              = "/aws/lambda/${var.function_name}"
   retention_in_days = var.cloudwatch_logs_retention_in_days
   kms_key_id        = var.cloudwatch_logs_kms_key_id
-
-  tags = merge(var.tags, var.cloudwatch_logs_tags)
 }
 
 resource "aws_ssm_parameter" "slack_token" {
